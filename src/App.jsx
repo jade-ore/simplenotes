4f66cd17d49941ea89af340fef34f885cb150f0f
jade_ore <jaydenthejayden1009@gmail.com>
2026-07-28 15:53:12 -0500

    create repo and initialization

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <p>hello</p> 
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Login.jsx
@@ -0,0 +1,10 @@
+import supabase from "./supabase";
+
+function Login() {
+    return (
+        <div>
+            <form>
+            </form>
+        </div>
+    )
+}
